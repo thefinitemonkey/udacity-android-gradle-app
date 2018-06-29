e5a61c14a64b7092e579334d9015ddf0ab480d23
Doug Brown <doug.brown@academy.com>
2018-06-29 15:08:47 -0500

    feat: Add joke service to GCE

---
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/Joke.java
@@ -0,0 +1,13 @@
+package com.udacity.gradle.builditbigger.backend;
+
+public class Joke {
+    private String mJoke;
+
+    public String getJoke() {
+        return mJoke;
+    }
+
+    public void setJoke(String joke) {
+        mJoke = joke;
+    }
+}
